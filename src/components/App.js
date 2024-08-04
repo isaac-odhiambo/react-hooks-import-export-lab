ae19c60e324a9730ef820fd644a1d167ec21f03f
Isaac Odiwuor Odhiambo <odhiamboisaach@gmail.com>
2024-08-04 19:02:28 +0300

    react

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,9 @@
 import React from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
+import { username,city} from "../data/user";
+
 
 function App() {
   return (
